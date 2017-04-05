7b8395d9a1bc38a1bcf4354f02edcc475abb093d
github username <github email address>
2017-04-05 08:40:00 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,11 +4,11 @@ require_relative 'models/textanalyzer.rb'
 class App < Sinatra::Base
 
 get '/' do
-  erb :index
+ erb :index
 end
 
 post '/' do
-  text_from_user = params[:user_text]
+  @analyzed_text = TextAnalyzer.new(params[:user_text])
   erb :results
 end
 
